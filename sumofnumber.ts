9d53e433453f9667acb91185ff90c60f9288396a
Jingyu Huang <156954674+jingyuh1@users.noreply.github.com>
2024-09-21 18:00:17 -1000

    Update sumofnumber.ts

--- sumofnumber.ts
+++ sumofnumber.ts
@@ -1,1 +1,30 @@
+const testNums: number[] = [1, 2, 3, 4];
 
+function sumFor(nums: number[]): number {
+    let total: number = 0;
+    for (const num of nums) {
+        total += num;
+    }
+    return total;
+}
+
+console.log(sumFor(testNums));
+
+const sumWhile = (nums: number[]): number => {
+    let total: number = 0;
+    let i: number = 0;
+    while (i < nums.length) {
+        total += nums[i];
+        i++;
+    }
+    return total;
+};
+
+console.log(sumWhile(testNums));
+
+function sumRecursion(nums: number[]): number {
+    if (nums.length === 0) {
+        return 0;
+    }
+    return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
